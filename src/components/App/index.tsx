84d1e3c868a7a1465b29819e0addec9c6ced62dd
Grgur Grisogono <grgur@grgur.com>
2019-03-06 11:15:45 +0100

    hooks ftw

---
+++ src/components/App/index.tsx
@@ -0,0 +1,23 @@
+import React, { lazy, Suspense } from 'react';
+import { IonApp, IonPage, IonContent } from '@ionic/react';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import './theme.css';
+
+const ModiteList = lazy(() =>
+  import('../ModiteList' /* webpackChunkName: "modite-list" */)
+);
+
+function App() {
+  return (
+    <IonApp>
+      <Suspense fallback={<IonContent>Loading...</IonContent>}>
+        <IonPage>
+          <ModiteList />
+        </IonPage>
+      </Suspense>
+    </IonApp>
+  );
+}
+
+export default App;
--- src/components/ModiteList/index.tsx
+++ src/components/ModiteList/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, FunctionComponent } from 'react';
 import {
   IonList,
   IonContent,
@@ -22,65 +22,72 @@ type Modite = {
   };
 };
 
-function renderlistItems(list: Modite[], filter: string) {
-  return list
-    .filter(
-      modite =>
-        modite.real_name.toLowerCase().indexOf(filter.toLowerCase()) > -1
-    )
-    .map(modite => (
-      <IonMenuToggle key={modite.id} auto-hide="false">
-        <IonItem button onClick={() => alert(modite.real_name)}>
-          <IonThumbnail slot="start">
-            <IonImg src={modite.profile.image_72} class={s.thumbnail} />
-          </IonThumbnail>
+type ListItemProps = {
+  list: Modite[];
+  filter: string;
+};
 
-          <IonLabel>{modite.real_name}</IonLabel>
-        </IonItem>
-      </IonMenuToggle>
-    ));
-}
+const ListItem: FunctionComponent<ListItemProps> = ({ list, filter }) => (
+  <>
+    {list
+      .filter(
+        modite =>
+          modite.real_name.toLowerCase().indexOf(filter.toLowerCase()) > -1
+      )
+      .map(modite => (
+        <IonMenuToggle key={modite.id} auto-hide="false">
+          <IonItem button onClick={() => alert(modite.real_name)}>
+            <IonThumbnail slot="start">
+              <IonImg src={modite.profile.image_72} class={s.thumbnail} />
+            </IonThumbnail>
 
-class ModiteList extends Component {
-  state = {
-    modites: [],
-    filter: '',
-  };
+            <IonLabel>{modite.real_name}</IonLabel>
+          </IonItem>
+        </IonMenuToggle>
+      ))}
+  </>
+);
 
-  componentDidMount() {
+function ModiteList() {
+  const [modites, setModites] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    // if we already have something, we can safely abandon fetching
+    if (modites.length) return;
+
     fetch('https://mosquito-slack-bot.herokuapp.com/modites')
       .then(res => res.json())
 
       // sort by last name
       .then(modites => {
-        this.setState({
-          modites: modites.sort((prev: Modite, next: Modite) => {
+        setModites(
+          modites.sort((prev: Modite, next: Modite) => {
             const prevName = prev.profile.last_name;
             const nextName = next.profile.last_name;
 
             if (prevName < nextName) return -1;
             if (prevName > nextName) return 1;
             return 0;
-          }),
-        });
+          })
+        );
       });
-  }
+  });
 
-  render() {
-    return (
-      <IonContent>
-        <IonSearchbar
-          debounce={500}
-          placeholder="Filter Modites"
-          onIonChange={event => this.setState({ filter: event.detail.value })}
-        />
-        <IonList>
-          <IonListHeader>Navigate</IonListHeader>
-          {renderlistItems(this.state.modites, this.state.filter)}
-        </IonList>
-      </IonContent>
-    );
-  }
+  return (
+    <IonContent>
+      <IonSearchbar
+        debounce={500}
+        placeholder="Filter Modites"
+        onIonChange={event => setFilter(event.detail.value || '')}
+        value={filter}
+      />
+      <IonList>
+        <IonListHeader>Modites</IonListHeader>
+        <ListItem list={modites} filter={filter} />
+      </IonList>
+    </IonContent>
+  );
 }
 
 export default ModiteList;
