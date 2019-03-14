42cd99797842801e474a04bf76bde74f9aeb9306
Grgur Grisogono <grgur@grgur.com>
2019-03-14 22:10:06 +0100

    Cloudflare setup
    API Endpoints:
    * https://modus.app/modites/all - get all modites
    
    Workflow:
    * update the modites.js function
    * it’s ok to create and import modules
    * run `npm run build`
    * run `npm run preview` to preview the script in Playground
    * deploy `dist/build.js` when ready
    
    Note:
    * `.env` file is not included
    * The serverless framework is set up, but it throws an error when deploying

---
+++ api/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: {
+    bundle: path.join(__dirname, './modites.js'),
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+
+  mode: process.env.NODE_ENV || 'production',
+
+  watchOptions: {
+    ignored: /node_modules|dist|\.js/g,
+  },
+
+  devtool: false, // CF recommends: 'cheap-module-eval-source-map',
+
+  optimization: {
+    minimize: false,
+  },
+
+  node: {
+    fs: 'empty',
+  },
+
+  plugins: [new Dotenv()],
+
+  target: 'webworker',
+};
