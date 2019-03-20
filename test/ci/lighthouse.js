22fc9b720023a937657b347840092fd135711b14
Grgur Grisogono <grgur@grgur.com>
2019-03-20 12:14:50 +0100

    Try 1

---
+++ test/ci/lighthouse.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const bot = require('circle-github-bot').create();
+const report = require('../../artifacts/lighthouse.report.json');
+
+const scores = Object.values(report.categories).reduce(
+  (agg, category) => `${agg}<h4>${category.title}</h4><div>${category.score}</div>`,
+  '',
+);
+
+bot.comment(`
+<h3>Lighthouse report</h3>
+
+${scores}
+
+<h4>Full Report</h4>
+<strong>${bot.artifactLink('artifacts/lighthouse.report.html', 'Lighthouse Report')}</strong>
+`);
