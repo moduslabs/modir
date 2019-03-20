267cd9e0fd98699e8d8e8dba429b1cbffaaf6788
Grgur Grisogono <grgur@grgur.com>
2019-03-20 12:32:52 +0100

    syntax fix

--- test/ci/lighthouse.js
+++ test/ci/lighthouse.js
@@ -8,11 +8,14 @@ const scores = Object.values(report.categories).reduce(
   '',
 );
 
-bot.comment((process.env.GH_AUTH_TOKEN, `
+bot.comment(
+  process.env.GH_AUTH_TOKEN,
+  `
 <h3>Lighthouse report</h3>
 
 ${scores}
 
 <h4>Full Report</h4>
 <strong>${bot.artifactLink('artifacts/lighthouse.report.html', 'Lighthouse Report')}</strong>
-`);
+`,
+);
