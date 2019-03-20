655affe16a567e914ce570ec78dc4df784984798
Grgur Grisogono <grgur@grgur.com>
2019-03-20 10:28:04 +0100

    fixed caching strategy

--- src/serviceWorkerCustom.js
+++ src/serviceWorkerCustom.js
@@ -1,5 +1,16 @@
+// uncoment for debugging
+// workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+
 workbox.routing.registerRoute(
   new RegExp('https://avatars.slack-edge.com/*'),
-  workbox.strategies.StaleWhileRevalidate()
-)
+  new workbox.strategies.StaleWhileRevalidate(),
+);
+
+// right now these are precached so this route makes no difference.
+// keeping it for future use
+workbox.routing.registerRoute(
+  new RegExp('/static/(css|js|media)/'),
+  new workbox.strategies.CacheFirst(),
+);
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
