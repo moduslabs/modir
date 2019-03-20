b74649b3300ad0da1204e769fe02cded5c32cd8e
Grgur Grisogono <grgur@grgur.com>
2019-03-20 12:55:04 +0100

    formatting fix

--- test/ci/lighthouse.js
+++ test/ci/lighthouse.js
@@ -4,9 +4,8 @@ const bot = require('circle-github-bot').create();
 const report = require('../../artifacts/lighthouse.report.json');
 
 const scores = Object.values(report.categories).reduce(
-  (agg, category) => `
-    ${agg}
-    | ${category.title} | ${~~category.score * 100} |`,
+  (agg, category) => `${agg}
+| ${category.title} | ${category.score * 100} |`,
   '',
 );
 
@@ -14,11 +13,14 @@ bot.comment(
   process.env.GH_AUTH_TOKEN,
   `
 <h3>Lighthouse report</h3>
+
 | Category        | Score           |
-| --------------- |:---------------:|
+| --------------- |----------------:|
 ${scores}
 
 <h4>Full Report</h4>
-<strong>${bot.artifactLink('artifacts/lighthouse.report.html', 'Lighthouse Report')}</strong>
+<strong>${bot
+    .artifactLink('artifacts/lighthouse.report.html', 'Lighthouse Report')
+    .replace('/project/', '/repo/')}</strong>
 `,
 );
