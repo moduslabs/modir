3938c39a7fb32bc4a793559e3958be5bd7804af7
Seth Lemmons <sethlemmons@hotmail.com>
2019-03-21 14:59:24 -0600

    Update the imports to be leaner importing only the map components needed

--- src/components/MapComponent/index.tsx
+++ src/components/MapComponent/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
-import * as am4core from '@amcharts/amcharts4/core';
-import * as am4maps from '@amcharts/amcharts4/maps';
+import { Circle, color, create } from '@amcharts/amcharts4/core';
+import { MapChart, projections, MapPolygonSeries, MapImageSeries } from '@amcharts/amcharts4/maps';
 import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
 import MapComponentProps from './MapComponentProps';
 import { defaultModite } from '../../models/Modite';
@@ -8,17 +8,17 @@ import s from './styles.module.css';
 
 function MapComponent({ modite = defaultModite }: MapComponentProps) {
   const mapRef: React.MutableRefObject<null> = useRef(null);
-  let map: am4maps.MapChart;
+  let map: MapChart;
 
   useEffect(() => {
     const { locationData = {} } = modite.profile.fields;
 
     if (!map && mapRef.current) {
       const el: any = mapRef.current;
-      map = am4core.create(el, am4maps.MapChart);
+      map = create(el, MapChart);
       map.geodata = am4geodata_worldLow;
-      map.projection = new am4maps.projections.Miller();
-      const polygonSeries = map.series.push(new am4maps.MapPolygonSeries());
+      map.projection = new projections.Miller();
+      const polygonSeries = map.series.push(new MapPolygonSeries());
       polygonSeries.useGeodata = true;
       polygonSeries.exclude = ["AQ"];
 
@@ -31,11 +31,11 @@ function MapComponent({ modite = defaultModite }: MapComponentProps) {
       // Configure series
       const polygonTemplate = polygonSeries.mapPolygons.template;
       polygonTemplate.tooltipText = "{name}";
-      polygonTemplate.fill = am4core.color("#ff5958");
+      polygonTemplate.fill = color("#ff5958");
 
       // Create hover state and set alternative fill color
       const hs = polygonTemplate.states.create("hover");
-      hs.properties.fill = am4core.color("#de4948");
+      hs.properties.fill = color("#de4948");
 
       polygonTemplate.events.on("hit", function (event: any) {
         map.maxZoomLevel = 1;
@@ -43,14 +43,14 @@ function MapComponent({ modite = defaultModite }: MapComponentProps) {
       });
 
       // Create image series
-      var imageSeries = map.series.push(new am4maps.MapImageSeries());
+      var imageSeries = map.series.push(new MapImageSeries());
 
       // Create a circle image in image series template so it gets replicated to all new images
       var imageSeriesTemplate = imageSeries.mapImages.template;
-      var circle = imageSeriesTemplate.createChild(am4core.Circle);
+      var circle = imageSeriesTemplate.createChild(Circle);
       circle.radius = 10;
-      circle.fill = am4core.color("#000000");
-      circle.stroke = am4core.color("#FFFFFF");
+      circle.fill = color("#000000");
+      circle.stroke = color("#FFFFFF");
       circle.strokeWidth = 4;
       circle.nonScaling = true;
       circle.tooltipText = "{title}";
