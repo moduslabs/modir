c73fb96586bf593f256f829d5e536df744557e8a
Grgur Grisogono <grgur@grgur.com>
2019-03-25 17:58:38 +0100

    Remove unneeded JS from bundle (#14)
    * Remove unneeded JS from bundle
    
    * remove `pdfmake` from amcharts
    * don’t eslint in webpack
    
    todo:
    * setup eslint and prettier hooks
    * setup dll
    * use cache-loader for babel
    
    * hard source plugin

--- config-overrides.js
+++ config-overrides.js
@@ -7,6 +7,7 @@ module.exports = function override(config, env) {
     config.output.globalObject = 'this';
   }
 
+  // custom service workers
   config.plugins = config.plugins.map(plugin => {
     if (plugin.constructor.name === 'GenerateSW') {
       return new WorkboxWebpackPlugin.InjectManifest({
@@ -18,7 +19,48 @@ module.exports = function override(config, env) {
     return plugin;
   });
 
+  // custom alias for src/
   config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
+  // remove eslint plugin (index #1)
+  config.module.rules.splice(1, 1);
+
+  // remove pdfmake from amcharts
+  config.externals = [
+    /(xlsx|canvg)/,
+    function(context, request, callback) {
+      if (/(pdfmake)/.test(request)) {
+        return callback(null, 'commonjs ' + request);
+      }
+
+      callback();
+    },
+  ];
+
+  // Webpack 5 will introduce something like this plugin
+  // Testing this instead of creating a DLL setup
+  const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+  config.plugins.push(new HardSourceWebpackPlugin());
+  config.plugins.push(
+    new HardSourceWebpackPlugin.ExcludeModulePlugin([
+      {
+        // HardSource works with mini-css-extract-plugin but due to how
+        // mini-css emits assets, assets are not emitted on repeated builds with
+        // mini-css and hard-source together. Ignoring the mini-css loader
+        // modules, but not the other css loader modules, excludes the modules
+        // that mini-css needs rebuilt to output assets every time.
+        test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+      },
+    ]),
+  );
+
+  // use this code to quickly analyze current config
+  // console.log(config.module.rules);
+  // throw new Error('stop');
+  // debugger;
+
+  // todo: add cache-loader to babel; add DLL
+  // config.module.rules[1].oneOf.map()
 
   return config;
 };
