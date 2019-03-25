dbeeb12024e4cf34f8aa694cbe6b57aee72a9a7f
Grgur Grisogono <grgur@grgur.com>
2019-03-25 17:55:04 +0100

    hard source plugin

--- config-overrides.js
+++ config-overrides.js
@@ -37,6 +37,23 @@ module.exports = function override(config, env) {
     },
   ];
 
+  // Webpack 5 will introduce something like this plugin
+  // Testing this instead of creating a DLL setup
+  const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+  config.plugins.push(new HardSourceWebpackPlugin());
+  config.plugins.push(
+    new HardSourceWebpackPlugin.ExcludeModulePlugin([
+      {
+        // HardSource works with mini-css-extract-plugin but due to how
+        // mini-css emits assets, assets are not emitted on repeated builds with
+        // mini-css and hard-source together. Ignoring the mini-css loader
+        // modules, but not the other css loader modules, excludes the modules
+        // that mini-css needs rebuilt to output assets every time.
+        test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+      },
+    ]),
+  );
+
   // use this code to quickly analyze current config
   // console.log(config.module.rules);
   // throw new Error('stop');
