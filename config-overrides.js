2b55eeff37c5e767d47a91e8d6771efa6041b9be
Grgur Grisogono <grgur@grgur.com>
2019-03-25 17:38:56 +0100

    Remove unneeded JS from bundle
    * remove `pdfmake` from amcharts
    * don’t eslint in webpack
    
    todo:
    * setup eslint and prettier hooks
    * setup dll
    * use cache-loader for babel

--- config-overrides.js
+++ config-overrides.js
@@ -7,6 +7,7 @@ module.exports = function override(config, env) {
     config.output.globalObject = 'this';
   }
 
+  // custom service workers
   config.plugins = config.plugins.map(plugin => {
     if (plugin.constructor.name === 'GenerateSW') {
       return new WorkboxWebpackPlugin.InjectManifest({
@@ -18,7 +19,31 @@ module.exports = function override(config, env) {
     return plugin;
   });
 
+  // custom alias for src/
   config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
+  // remove eslint plugin (index #1)
+  config.module.rules.splice(1, 1);
+
+  // remove pdfmake from amcharts
+  config.externals = [
+    /(xlsx|canvg)/,
+    function(context, request, callback) {
+      if (/(pdfmake)/.test(request)) {
+        return callback(null, 'commonjs ' + request);
+      }
+
+      callback();
+    },
+  ];
+
+  // use this code to quickly analyze current config
+  // console.log(config.module.rules);
+  // throw new Error('stop');
+  // debugger;
+
+  // todo: add cache-loader to babel; add DLL
+  // config.module.rules[1].oneOf.map()
 
   return config;
 };
