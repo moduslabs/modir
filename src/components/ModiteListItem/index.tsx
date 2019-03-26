d14e0a73381385e4f4a7739fc561507dccac6a60
Grgur Grisogono <grgur@grgur.com>
2019-03-26 13:14:09 +0100

    Change route and then fetch
    This way we let the browser to some work while the API is doing its thing

--- src/components/ModiteListItem/index.tsx
+++ src/components/ModiteListItem/index.tsx
@@ -1,24 +1,27 @@
-import React, { FunctionComponent, useContext } from "react";
-import ListItemProps from "../../models/ListItemProps";
-import { RouteComponentProps, withRouter } from "react-router";
-import Modite, { defaultModite } from "../../models/Modite";
-import ModiteContext from "../../state/modite";
-import ModiteProfileResp from "../../models/ModiteProfileResp";
-import { IonMenuToggle, IonItem, IonThumbnail, IonLabel } from "@ionic/react";
-import ModiteImage from "../ModiteImage";
+import React, { FunctionComponent, useContext } from 'react';
+import ListItemProps from '../../models/ListItemProps';
+import { RouteComponentProps, withRouter } from 'react-router';
+import Modite, { defaultModite } from '../../models/Modite';
+import ModiteContext from '../../state/modite';
+import ModiteProfileResp from '../../models/ModiteProfileResp';
+import { IonMenuToggle, IonItem, IonThumbnail, IonLabel } from '@ionic/react';
+import ModiteImage from '../ModiteImage';
 import s from './styles.module.css';
 
-const ModiteListItem: FunctionComponent<ListItemProps & RouteComponentProps> = ({ modite, history }) => {
+const ModiteListItem: FunctionComponent<ListItemProps & RouteComponentProps> = ({
+  modite,
+  history,
+}) => {
   const [activeModite, setActiveModite]: [Modite, React.Dispatch<any>] = useContext(ModiteContext);
   const handleItemClick = async (): Promise<void> => {
     if (modite.id !== activeModite.id) {
       setActiveModite(defaultModite);
+      history.push(`/details/${modite.id}`);
       const moditeProfile: ModiteProfileResp = await fetch(
         `https://modus.app/modite/${modite.id}`,
       ).then(res => res.json());
       if (moditeProfile.ok) modite.profile = moditeProfile.profile;
       setActiveModite(modite);
-      history.push(`/details/${modite.id}`);
     }
   };
 
