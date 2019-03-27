eb9978041c05f6cccd22ee5234d05b44670a3c11
Grgur Grisogono <grgur@grgur.com>
2019-03-27 09:39:56 +0100

    Only zoom if there is lat/long

--- src/components/MapComponent/index.tsx
+++ src/components/MapComponent/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { Circle, color, create } from '@amcharts/amcharts4/core';
 import { MapChart, projections, MapPolygonSeries, MapImageSeries } from '@amcharts/amcharts4/maps';
-import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+import am4geodata_worldLow from '@amcharts/amcharts4-geodata/worldLow';
 import MapComponentProps from './MapComponentProps';
 import { defaultModite } from '../../models/Modite';
 import s from './styles.module.css';
@@ -20,24 +20,24 @@ function MapComponent({ modite = defaultModite }: MapComponentProps) {
       map.projection = new projections.Miller();
       const polygonSeries = map.series.push(new MapPolygonSeries());
       polygonSeries.useGeodata = true;
-      polygonSeries.exclude = ["AQ"];
+      polygonSeries.exclude = ['AQ'];
 
       map.seriesContainer.draggable = false;
       map.seriesContainer.resizable = false;
       // map.maxZoomLevel = 1;
-      map.seriesContainer.events.disableType("doublehit");
-      map.chartContainer.background.events.disableType("doublehit");
+      map.seriesContainer.events.disableType('doublehit');
+      map.chartContainer.background.events.disableType('doublehit');
 
       // Configure series
       const polygonTemplate = polygonSeries.mapPolygons.template;
-      polygonTemplate.tooltipText = "{name}";
-      polygonTemplate.fill = color("#d6d6d6");
+      polygonTemplate.tooltipText = '{name}';
+      polygonTemplate.fill = color('#d6d6d6');
 
       // Create hover state and set alternative fill color
-      const hs = polygonTemplate.states.create("hover");
-      hs.properties.fill = color("#d6d6d6");
+      const hs = polygonTemplate.states.create('hover');
+      hs.properties.fill = color('#d6d6d6');
 
-      polygonTemplate.events.on("hit", function (event: any) {
+      polygonTemplate.events.on('hit', function(event: any) {
         map.maxZoomLevel = 1;
         event.target.isActive = false;
       });
@@ -49,15 +49,15 @@ function MapComponent({ modite = defaultModite }: MapComponentProps) {
       var imageSeriesTemplate = imageSeries.mapImages.template;
       var circle = imageSeriesTemplate.createChild(Circle);
       circle.radius = 10;
-      circle.fill = color("#ff5c5d");
-      circle.stroke = color("#FFFFFF");
+      circle.fill = color('#ff5c5d');
+      circle.stroke = color('#FFFFFF');
       circle.strokeWidth = 4;
       circle.nonScaling = true;
-      circle.tooltipText = "{title}";
+      circle.tooltipText = '{title}';
 
       // Set property fields
-      imageSeriesTemplate.propertyFields.latitude = "latitude";
-      imageSeriesTemplate.propertyFields.longitude = "longitude";
+      imageSeriesTemplate.propertyFields.latitude = 'latitude';
+      imageSeriesTemplate.propertyFields.longitude = 'longitude';
 
       if (locationData) {
         // Add user location by lat / lon
@@ -65,19 +65,18 @@ function MapComponent({ modite = defaultModite }: MapComponentProps) {
         let { lat: latitude, lon: longitude } = locationData;
 
         map.events.on('ready', () => {
-          setTimeout(() => {
-            map.zoomToGeoPoint({ latitude, longitude }, 5, true, 500);
-            imageSeries.data = [{ latitude, longitude, title }];
-          }, 100);
+          if (latitude && longitude) {
+            requestAnimationFrame(() => {
+              map.zoomToGeoPoint({ latitude, longitude }, 5, true, 500);
+              imageSeries.data = [{ latitude, longitude, title }];
+            });
+          }
         });
       }
-
     }
   });
 
-  return (
-    <div className={s.mapCt} ref={mapRef}></div>
-  );
+  return <div className={s.mapCt} ref={mapRef} />;
 }
 
 export default MapComponent;
