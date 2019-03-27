0264b2db2de9f6bdbfb67aa9c16fd3cb934267df
Grgur Grisogono <grgur@grgur.com>
2019-03-27 16:42:58 +0100

    offline sw capabilities

--- src/serviceWorkerCustom.js
+++ src/serviceWorkerCustom.js
@@ -1,6 +1,29 @@
 // uncoment for debugging
 // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
+const cachedResponseWillBeUsed = ({ cache, request, cachedResponse }) => {
+  // If there's already a match against the request URL, return it.
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+
+  // Otherwise, return a match for a specific URL:
+  const urlToMatch = 'index.html';
+  return caches.match(urlToMatch);
+};
+
+// cache URL parameters such as ?moduslabsci
+const indexCachingStrategy = workbox.strategies.networkFirst({
+  cacheName: 'modir-index',
+  cacheExpiration: {
+    maxEntries: 5,
+  },
+  cacheableResponse: { statuses: [0, 200] },
+  plugins: [{ cachedResponseWillBeUsed }],
+});
+
+workbox.routing.registerRoute(/\.(?:html|png)$/, indexCachingStrategy);
+
 workbox.routing.registerRoute(
   new RegExp('https://avatars.slack-edge.com/*'),
   new workbox.strategies.StaleWhileRevalidate(),
@@ -13,4 +36,6 @@ workbox.routing.registerRoute(
   new workbox.strategies.CacheFirst(),
 );
 
-workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [], {
+  offlinePage: '/index.html', //<- in case of getting offline and not have cache content , redirect here
+});
