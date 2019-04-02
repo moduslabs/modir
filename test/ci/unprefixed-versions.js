c2d5e6dcfe96fff483dcdd54defd4fa1f04f1ed9
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-04-02 09:05:50 -0400

    Fix check version

--- test/ci/unprefixed-versions.js
+++ test/ci/unprefixed-versions.js
@@ -1,46 +1,37 @@
-// tslint:disable: no-console
-const path = require('path');
-const packageJson = require(path.join(__dirname, '../../package.json'));
-const props = [
-  'dependencies',
-  'devDependencies',
-  'optionalDependencies',
-  'peerDependencies,'
-];
+/* eslint-disable @typescript-eslint/no-var-requires,no-console */
+const path = require('path')
+const packageJson = require(path.join(__dirname, '../../package.json'))
+const props = ['dependencies', 'devDependencies', 'optionalDependencies', 'peerDependencies,']
 
-const UNPREFIXED_RE = /^\d/;
-const found = [];
+const UNPREFIXED_RE = /^\^|~/
+const found = []
 
 props.forEach(prop => {
-  const obj = packageJson[prop];
+  const obj = packageJson[prop]
 
   if (obj) {
     Object.keys(obj).forEach(name => {
-      const version = obj[name];
+      const version = obj[name]
 
-      if (!version.match(UNPREFIXED_RE)) {
+      if (version.match(UNPREFIXED_RE)) {
         found.push({
           name,
           prop,
-          version
-        });
+          version,
+        })
       }
     })
   }
-});
+})
 
 if (found.length) {
-  console.log(`FOUND ${found.length} UNPREFIXED VERSION${found.length === 1 ? '' : 'S'}!`);
+  console.log(`FOUND ${found.length} UNPREFIXED VERSION${found.length === 1 ? '' : 'S'}!`)
 
-  found.forEach(({
-    name,
-    prop,
-    version
-  }) => console.log(`  ${name} -> ${version} (in ${prop})`));
+  found.forEach(({ name, prop, version }) => console.log(`  ${name} -> ${version} (in ${prop})`))
 
-  console.log();
+  console.log()
 
-  process.exit(1);
+  process.exit(1)
 } else {
-  console.log('No prefixed versions found!');
+  console.log('No prefixed versions found!')
 }
