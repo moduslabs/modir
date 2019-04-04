e4fc435e970c8f83f50e58ecead44aff53b7f35b
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-04-04 16:28:18 -0400

    Split out detail views and list items

--- src/components/DetailsView/index.tsx
+++ src/components/DetailsView/index.tsx
@@ -5,6 +5,8 @@ import s from './styles.module.css'
 import VirtualizedList from '../VirtualizedList'
 import Project from '../../models/Project'
 import { DataState } from '../../types/service/Data'
+import { IonIcon } from '@ionic/react'
+import Modite from '../../models/Modite'
 
 let lastScrollOffset = 0 // used by onScroll
 
@@ -13,63 +15,95 @@ const onScroll = ({ scrollOffset }: { scrollOffset: number }): void => {
   lastScrollOffset = scrollOffset
 }
 
-// TODO: type correctly
-function DetailsView({ className = '' }: any) {
-  const [{ activeModite, activeProject }]: [DataState] = useContext(DataContext)
-  const isProject = Boolean(activeProject)
-  const activeItem = isProject ? activeProject : activeModite
+const NAME_CHECKER_RE = /[@/]/
+const NAME_REPLACER_RE = /([@/]?)(.+)$/
 
-  if (!activeItem) {
+const GitHub = ({ className, name }: { className?: string; name?: string }) => {
+  if (!name) {
     return null
   }
 
-  const { profile = {}, users = [] }: any = activeItem
-  const { fields = {}, title } = profile
+  if (name.match(NAME_CHECKER_RE)) {
+    name = name.replace(NAME_REPLACER_RE, '$2')
+  }
 
-  const userCount = users.length
-  const image = profile.image_192
-  const { Location: location, Title, 'GitHub User': gitHubUser } = fields
-  const { tod, localDate, localTime } = activeItem
-  const name = activeItem.real_name || activeItem.name
+  return (
+    <div className={className}>
+      <a href={`https://github.com/${name}`} target="_blank" rel="noopener noreferrer">
+        <IonIcon name="logo-github" />
+        {name}
+      </a>
+    </div>
+  )
+}
+
+const ModiteDetail = ({ className, modite }: { className?: string; modite?: Modite | null }) => {
+  if (!modite) {
+    return null
+  }
+
+  const { localDate, localTime, profile = {}, real_name: name, tod }: any = modite
+  const { fields = {}, image_192: image, title } = profile
+  const { Location, Title, 'GitHub User': gitHubUser } = fields
 
   const cx = classNames.bind(s)
-  className = cx('moditeCt', className, { isModite: !isProject, isProject: isProject })
-  const moditeDetailsWrapCLs = cx('moditeDetails', { moditeDetailsShown: !isProject })
-  const projectDetailsWrapCLs = cx('projectDetails', { projectDetailsShown: isProject })
+  className = cx(s.moditeCt, className, s.isModite)
+  const moditeDetailsWrapCLs = cx(s.moditeDetails, s.moditeDetailsShown)
 
   return (
     <div className={className}>
       {image && <img src={image} />}
-      {isProject ? (
-        <div className={projectDetailsWrapCLs}>
-          <div className={s.name}>{name}</div>
+      <div className={moditeDetailsWrapCLs}>
+        <div className={s.name}>{name}</div>
 
-          <div className={s.userCount}>{userCount}</div>
-
-          <VirtualizedList
-            records={(activeItem as Project).users}
-            onScroll={onScroll}
-            initialScrollOffset={lastScrollOffset}
-          />
+        <div className={s.location}>{Location}</div>
+        <div className={s.todWrap}>
+          <span className={s.tod}>{tod}</span>
+          {localDate} {localDate && localTime && '-'} {localTime}
         </div>
-      ) : (
-        <div className={moditeDetailsWrapCLs}>
-          <div className={s.name}>{name}</div>
 
-          <div className={s.location}>{location}</div>
-          <div className={s.todWrap}>
-            <span className={s.tod}>{tod}</span>
-            {localDate} {localDate && localTime && '-'} {localTime}
-          </div>
+        <div className={s.fieldTitle}>{Title}</div>
+        <GitHub className={s.gitUser} name={gitHubUser} />
 
-          <div className={s.fieldTitle}>{Title}</div>
-          <div className={s.gitUser}>{gitHubUser}</div>
-
-          <div className={s.title}>{title}</div>
-        </div>
-      )}
+        <div className={s.title}>{title}</div>
+      </div>
     </div>
   )
+}
+
+const ProjectDetail = ({ className, project }: { className?: string; project?: Project | null }) => {
+  if (!project) {
+    return null
+  }
+
+  const { name, users } = project
+  const userCount = users.length
+
+  const cx = classNames.bind(s)
+  className = cx(s.moditeCt, className, s.isProject)
+  const projectDetailsWrapCLs = cx(s.projectDetails, s.projectDetailsShown)
+
+  return (
+    <div className={className}>
+      <div className={projectDetailsWrapCLs}>
+        <div className={s.name}>{name}</div>
+
+        <div className={s.userCount}>{userCount}</div>
+
+        <VirtualizedList records={users} onScroll={onScroll} initialScrollOffset={lastScrollOffset} />
+      </div>
+    </div>
+  )
+}
+
+function DetailsView({ className }: { className?: string }) {
+  const [{ activeModite, activeProject }]: [DataState] = useContext(DataContext)
+
+  if (activeProject) {
+    return <ProjectDetail className={className} project={activeProject} />
+  } else {
+    return <ModiteDetail className={className} modite={activeModite} />
+  }
 }
 
 export default DetailsView
--- src/models/ListItemProps.ts
+++ src/models/ListItemProps.ts
@@ -2,5 +2,5 @@ import Modite from './Modite'
 import Project from './Project'
 
 export default interface ListItemProps {
-  modite: Modite | Project
+  item: Modite | Project
 }
