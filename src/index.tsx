90c5eebc176507790394f59865cae0cd75e3b8b0
Grgur Grisogono <grgur@grgur.com>
2019-04-08 10:33:40 +0200

    try with Cloudflare auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import initAuth from './auth'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
-function init() {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
-
-if (document.location.search.match(/moduslabsci/)) {
-  init()
-} else {
-  initAuth(init)
-}
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
