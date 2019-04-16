9dc805016a4e94e7c8d6e0bef9bfd76566a22454
Grgur Grisogono <grgur@grgur.com>
2019-04-16 15:06:33 +0200

    Calculate time asynchronously on raf

--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -1,33 +1,51 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
+import cx from 'classnames'
 import Modite from '../../models/Modite'
 import s from './styles.module.css'
 
+interface TimeData {
+  time?: string
+  tod?: string
+}
+
 const RawTime = ({ modite, date }: { modite: Modite; date?: boolean }) => {
-  const now = new Date()
-  const time = now.toLocaleString('en-US', {
-    timeZone: modite.tz,
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
+  const [data, setData]: [TimeData, React.Dispatch<any>] = useState({})
+
+  useEffect(() => {
+    const requestID = requestAnimationFrame(() => {
+      const now = new Date()
+      const time = now.toLocaleString('en-US', {
+        timeZone: modite.tz,
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+      })
+
+      const hour = Number(
+        now.toLocaleString('en-US', {
+          timeZone: modite.tz,
+          hour: 'numeric',
+          hour12: false,
+        }),
+      )
+
+      const isNight = hour < 8 || hour > 22
+      const tod: string = isNight ? '🌙' : '☀️'
+
+      setData({ tod, time })
+    })
+
+    return () => cancelAnimationFrame(requestID)
   })
-
-  const hour = Number(
-    now.toLocaleString('en-US', {
-      timeZone: modite.tz,
-      hour: 'numeric',
-      hour12: false,
-    }),
-  )
-
-  const isNight = hour < 8 || hour > 22
-  const tod: string = isNight ? '🌙' : '☀️'
 
   return (
     <>
-      <span aria-hidden="true">{tod}</span>
-      <time className={s.localTime} dateTime={time}>
+      <span aria-hidden="true" className={cx({ [s.appear]: !!data.tod })}>
+        {data.tod}
+      </span>
+      <time dateTime={data.time} className={cx(s.localTime, { [s.appear]: !!data.tod })}>
         {date ? `${modite.localDate} - ` : null}
-        {time}
+        {data.time}
       </time>
     </>
   )
