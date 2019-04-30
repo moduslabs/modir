7a5620cfed7a64e923c245afef8783141d195888
Grgur Grisogono <grgur@grgur.com>
2019-04-30 14:07:05 +0200

    attempt to fix 200 response

--- src/serviceWorkerCustom.js
+++ src/serviceWorkerCustom.js
@@ -2,35 +2,6 @@
 // uncoment for debugging
 // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
-const cachedResponseWillBeUsed = ({ cachedResponse }) => {
-  // If there's already a match against the request URL, return it.
-  if (cachedResponse) {
-    return cachedResponse
-  }
-
-  // Otherwise, return a match for a specific URL:
-  const urlToMatch = 'index.html'
-  return caches.match(urlToMatch)
-}
-
-// cache URL parameters such as ?moduslabsci
-const indexCachingStrategy = workbox.strategies.networkFirst({
-  cacheName: 'modir-index',
-  cacheExpiration: {
-    maxEntries: 5,
-  },
-  cacheableResponse: {
-    statuses: [0, 200],
-  },
-  plugins: [
-    {
-      cachedResponseWillBeUsed,
-    },
-  ],
-})
-
-workbox.routing.registerRoute(/\.(?:html|png)$/, indexCachingStrategy)
-
 workbox.routing.registerRoute(
   new RegExp('https://avatars.slack-edge.com/*'),
   new workbox.strategies.StaleWhileRevalidate(),
@@ -38,7 +9,16 @@ workbox.routing.registerRoute(
 
 // right now these are precached so this route makes no difference.
 // keeping it for future use
-workbox.routing.registerRoute(new RegExp('/static/(css|js|media)/'), new workbox.strategies.CacheFirst())
+workbox.routing.registerRoute(
+  new RegExp('/static/(css|js|media)/'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'statics',
+  }),
+)
+
+workbox.routing.registerRoute(new RegExp('/.(?:html)$/'), new workbox.strategies.NetworkFirst())
+
+workbox.routing.registerRoute(new RegExp('^https://dir.modus.app/'), new workbox.strategies.NetworkFirst())
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [], {
   offlinePage: '/index.html', //<- in case of getting offline and not have cache content , redirect here
