18dd1f20d69fd4860192df5682c4c29266bcbea4
Abraham Arias <arias.abram@gmail.com>
2019-05-09 15:35:33 -0700

    Removing identation space

--- test/AutomationTests/specs/Dashboard/dashboard.js
+++ test/AutomationTests/specs/Dashboard/dashboard.js
@@ -13,56 +13,56 @@ const projectsPage = new ProjectsPage();
 const ModiteProfilePage = require('../../pages/moditeProfile.page');
 const moditeProfilePage = new ModiteProfilePage();
 
-    describe('Dashboard UI TESTS', () => {
-        beforeEach(function() {
-            loginPage.open();
-            loginPage.doCompleteLogin('user@moduscreate.com', 'password');
-        });
-
-        it('should have the right title', () => {
-            assert.equal(loginPage.getTitle(), 'Modite Directory');
-        });
-
-        it('should have the search field present', () => {
-            expect(dashPage.isSearchInputPresent());
-        });
-
-        it('should have the Team button present', () => {
-            expect(dashPage.isTeamButtonPresent());
-        });
-
-        it('should have Projects button present', () => {
-            expect(dashPage.isProjectsButtonPresent());
-        })
+describe('Dashboard UI TESTS', () => {
+    beforeEach(function() {
+        loginPage.open();
+        loginPage.doCompleteLogin('user@moduscreate.com', 'password');
     });
 
-    describe('Dashboard Functional Tests', () => {
-        beforeEach(function() {
-            loginPage.open();
-            loginPage.doCompleteLogin('user@moduscreate.com', 'password');
-        });
+    it('should have the right title', () => {
+        assert.equal(loginPage.getTitle(), 'Modite Directory');
+    });
 
-        it('should be able to navigate to Projects Page', () => {
-            dashPage.clickProjectsButton();
-            expect(projectsPage.isProjectListPresent());
-        });
+    it('should have the search field present', () => {
+        expect(dashPage.isSearchInputPresent());
+    });
 
-        it('should be able to search a modite', () => {
-            dashPage.fillsInputModites("Clayton Dog");
+    it('should have the Team button present', () => {
+        expect(dashPage.isTeamButtonPresent());
+    });
 
-            expect(dashPage.resultListIsEmpty())to.equal(false);
-        });
+    it('should have Projects button present', () => {
+        expect(dashPage.isProjectsButtonPresent());
+    })
+});
 
-        it('should be able to navigate to modite profile page', () => {
-            dashPage.fillsInputModites("Clayton Dog");
-            dashPage.clickFirstModiteFromList();
+describe('Dashboard Functional Tests', () => {
+    beforeEach(function() {
+        loginPage.open();
+        loginPage.doCompleteLogin('user@moduscreate.com', 'password');
+    });
 
-            expect(moditeProfilePage.areAllUIElementsPresent());
-        });
+    it('should be able to navigate to Projects Page', () => {
+        dashPage.clickProjectsButton();
+        expect(projectsPage.isProjectListPresent());
+    });
 
-        it('should NOT be able to see any result', () => {
-            dashPage.fillsInputModites("NoExistingModite");
+    it('should be able to search a modite', () => {
+        dashPage.fillsInputModites("Clayton Dog");
 
-            expect(dashPage.resultListIsEmpty());
-        });
+        expect(dashPage.resultListIsEmpty())to.equal(false);
+    });
+
+    it('should be able to navigate to modite profile page', () => {
+        dashPage.fillsInputModites("Clayton Dog");
+        dashPage.clickFirstModiteFromList();
+
+        expect(moditeProfilePage.areAllUIElementsPresent());
+    });
+
+    it('should NOT be able to see any result', () => {
+        dashPage.fillsInputModites("NoExistingModite");
+
+        expect(dashPage.resultListIsEmpty());
+    });
 });
