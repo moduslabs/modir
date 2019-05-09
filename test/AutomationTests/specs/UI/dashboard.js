d49194b9f2d598bdbb90ab4d0bb3ece82932ac51
Abraham Arias <arias.abram@gmail.com>
2019-05-09 12:39:14 -0700

    Creating Dashboard UI expectation tests

--- test/AutomationTests/specs/UI/dashboard.js
+++ test/AutomationTests/specs/UI/dashboard.js
@@ -1,22 +1,31 @@
 const assert = require('assert');
+const expect = require('chai').expect;
+
 const LoginPage = require('../../pages/login.page');
 const loginPage = new LoginPage();
 
-describe('Dashboard', () => {
-  it('should have the right title', () => {
-    loginPage.open();
-    assert.equal(loginPage.getTitle(), 'Modite Directory')
-  });
+const DashPage = require('../../pages/dashboard.page');
+const dashPage = new DashPage();
 
-  it('should have the search field present', () => {
-      //#todo
-  });
+describe('Dashboard UI TESTS', () => {
+    beforeEach(function() {
+        loginPage.open();
+        loginPage.doCompleteLogin('user@moduscreate.com', 'password');
+    });
 
-  it('should have the Team button present', () => {
-      //#todo
-  });
+    it('should have the right title', () => {
+        assert.equal(loginPage.getTitle(), 'Modite Directory');
+    });
 
-  it('should have Projects button present', () => {
-      //#todo
-  })
+    it('should have the search field present', () => {
+        expect(dashPage.isSearchInputPresent());
+    });
+
+    it('should have the Team button present', () => {
+        expect(dashPage.isTeamButtonPresent());
+    });
+
+    it('should have Projects button present', () => {
+        expect(dashPage.isProjectsButtonPresent());
+    })
 });
