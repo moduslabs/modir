cefae21ed28d787d174e87159b982701920ffb52
Mitchell Simoens <mitchellsimoens@gmail.com>
2019-07-02 14:29:28 -0400

    disable serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -21,4 +21,4 @@ window.initApp = initApp
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.unregister()
