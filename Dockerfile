9f79bc4ab72375cb9241a613b539d7c7825919f2
Manuel Rodriguez <rodriguez.manu@gmail.com>
2021-05-11 18:56:30 -0300

    [WIP] Fix docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM node:12
+RUN yarn
+RUN npm install react-app-rewired -g --silent
 WORKDIR /home/app
 COPY . /home/app
 CMD [ "npm", "start" ]
--- docker-build.sh
+++ docker-build.sh
@@ -8,5 +8,6 @@ echo installing node_modules in volume modir-node_modules
 docker run \
   --rm \
   -v modir-node_modules:/home/app/node_modules \
-  modir \
-  npm install
+  -p 3000:3000 \
+  -e CHOKIDAR_USEPOLLING=true \
+  modir
