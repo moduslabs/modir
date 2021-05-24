5c9912c18346866d60c693a0822ce7fdf533508c
David Kezi <davidkezi6@gmail.com>
2021-05-24 18:26:31 +0100

    fix: add back arrow icon (#111)

--- src/components/App/icons.ts
+++ src/components/App/icons.ts
@@ -1,12 +1,12 @@
 import { addIcons } from 'ionicons'
-import { clipboard, globe, list, logoGithub, logoSkype, people } from 'ionicons/icons'
+import { clipboard, globe, list, logoGithub, logoSkype, people, arrowBack } from 'ionicons/icons'
 
 addIcons({
   clipboard,
   globe,
   list,
   people,
-
+  'arrow-back': arrowBack,
   'logo-github': logoGithub,
   'logo-skype': logoSkype,
 })
